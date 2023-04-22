47b7ce9bb920569c0ff0aee936c01c437c5ff320
indrayanikalake <indraayanikalaake333@gmail.com>
2023-04-22 23:54:21 +0530

    tag

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-var items=document.getElementsByClassName("list-group-item");
-console.log(items);
-console.log(items[1]);
-items[1].textContent='Hello World';
-items[1].style.fontsize='bold';
-//items[1].style.backgroundColor='yellow';
-//items,s.style.backgroundColor='#f4f4f4';
-for(let i=0;i<items.length;i++){
-    items[i].style.backgroundColor='#f4f4f4';
-}
-//question
-items[2].style.backgroundColor="green";
-for(let i=0;i<items.length;i++){
-    items[i].style.color='#00ff00';
-    items[i].style.fontsize='bold';
-}
+
+var li =document.getElementsByTagName("li");
+li[4].style.fontSize='bold';
+li[4].style.background='#f4f4f4';
+li[4].style.color="#00ff00";
