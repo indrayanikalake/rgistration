9a3b0211312c9a12f974be8a82047b42779241ee
indrayanikalake <indraayanikalaake333@gmail.com>
2023-04-22 20:17:23 +0530

    dom3

---
+++ dom3/q2.js
@@ -0,0 +1,16 @@
+//q.2 Manipulate the title of the page and experiment with other things.
+var headtitle=document.getElementById("header-title");
+console.log(headtitle);
+headtitle.textContent="Hello";
+headtitle.textContent="GoodBye";
+
+console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.links);
+console.log(document.images);
---
+++ dom3/q3.js
@@ -0,0 +1,6 @@
+//q3 make ADD ITEM bold and use black border and make the color green
+var titlehead=document.getElementById("t");
+titlehead.innerHTML='<h1>ADD ITEM</h1>';
+titlehead.style.color='green';
+var headertitle=document.getElementById("header-title");
+headertitle.style.borderBottom='solid 3px #000';
