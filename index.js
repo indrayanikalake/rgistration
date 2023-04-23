2258c4610b06c0086a0bb04c84e28b937907283e
indrayanikalake <indraayanikalaake333@gmail.com>
2023-04-23 13:56:55 +0530

    creating nodes and dom manipulation

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-
-//q1
-var secondItem=document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor='green';
-
-
-var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.visibility='hidden';
-
-//q2
-var odd=document.querySelectorAll('li:nth-child(odd)');
-
-for(let i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor="green";
-    
-}
-var secondItem=document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.color="green";
+//1 parentElemnt
+var itemlist=document.querySelector('#items');
+console.log(itemlist.parentElement);
+//2 lastelementchild
+console.log(itemlist.lastElementChild);
+itemlist.lastElementChild.textContent='hello4';
+//3 lastchild
+console.log(itemlist.lastChild);
+//5 firstelemnetchild
+console.log(itemlist.firstElementChild);
+itemlist.firstElementChild.textContent='hello1';
+//6 firstchild
+console.log(itemlist.firstChild);
+//7 nextsibling
+console.log(itemlist.nextSibling);
+//8 nextelementsibling
+console.log(itemlist.nextElementSibling);
+//9 prevsibling
+console.log(itemlist.previousSibling);
+//10 prevelementsibling
+console.log(itemlist.previousElementSibling);
+itemlist.previousElementSibling.style.color="green";
+//11 createelement
+var div=document.createElement('div');
+//12 setAttrribute
+div.setAttribute('title','hello div');
+//13 createTextNode
+var text=document.createTextNode('Hello World');
+//14 appendChild
+div.appendChild(text);
+console.log(div);
