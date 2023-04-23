b67cfd82a2f29f25574e49722daceb331e7b340d
indrayanikalake <indraayanikalaake333@gmail.com>
2023-04-23 11:49:58 +0530

    queryselector

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 
-var li =document.getElementsByTagName("li");
-li[4].style.fontSize='bold';
-li[4].style.background='#f4f4f4';
-li[4].style.color="#00ff00";
+//q1
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green';
+
+
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility='hidden';
+
+//q2
+var odd=document.querySelectorAll('li:nth-child(odd)');
+
+for(let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor="green";
+    
+}
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color="green";
